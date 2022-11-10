02b5234b95de16d75ecf5bdb77cafd386343fb34
KurodaKanbei <mistergalahad@gmail.com>
2022-11-10 20:47:52 +0100

    overhaul

--- pre.py
+++ pre.py
@@ -17,8 +17,8 @@ from sklearn.impute import KNNImputer
 from sklearn.impute import SimpleImputer
 from sklearn.datasets import dump_svmlight_file
 from xgboost import XGBRegressor
-from sklearn.feature_selection import SelectKBest, f_regression, mutual_info_regression, SelectFromModel
-from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.feature_selection import SelectKBest, f_regression, mutual_info_regression, SelectFromModel, RFE
+from sklearn.ensemble import ExtraTreesRegressor, GradientBoostingRegressor
 
 if __name__ == '__main__':
 
@@ -29,20 +29,94 @@ if __name__ == '__main__':
                     delimiter=',',
                     index_col='id')
 
+    idx = []
+    for i, feature in enumerate(X.columns):
+        values = X[feature].dropna(0)
+        mx = values.max()
+        mn = values.min()
+        if mn > 0 and abs((mx - mn) / mn - .1) < 0.05:
+            idx.append(feature)
+    X = X.drop(idx, axis=1)
+    print(X.shape)
     nan_idx = np.isnan(X)
     imputer = KNNImputer(n_neighbors=20)
+    # imputer = SimpleImputer(strategy='median')
     X_imp = imputer.fit_transform(X)
 
-    model = sklearn.ensemble.IsolationForest(max_samples=0.5, contamination=0.025)
-    # model = sklearn.neighbors.LocalOutlierFactor(contamination=0.025)
-    outl_pred = model.fit_predict(X_imp)
+    X_train = X_imp
+    y_train = y.to_numpy()
+
+    # lsvr = LinearSVR(C=0.01).fit(X_train, y_train)
+    # model = ExtraTreesRegressor(n_estimators=50).fit(X_train, y_train.ravel())
+    model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1,
+                                        max_depth=3, min_samples_leaf=15, min_samples_split=10)
+    selector = RFE(model, step=0.1, n_features_to_select=60, verbose=1)
+    # selector = RFECV(estimator, step=0.1,cv=5,min_features_to_select=50,verbose=1)
+    selector.fit(X_train, y_train.ravel())
+    # selector = SelectKBest(score_func=mutual_info_regression, k=200)
+    X_cleaned, y_cleaned = X_train, y_train
+    X_train = selector.transform(X_train)
+
+    model = sklearn.ensemble.IsolationForest(contamination="auto")
+    outl_pred = model.fit_predict(X_train)
     mask = outl_pred != -1
 
+    X_train, y_train = X_train[mask, :], y_train[mask]
 
-    X_cleaned = np.copy(X_imp)
-    y_cleaned = np.copy(y.values)
-    X_cleaned, y_cleaned = X_cleaned[mask, :], y_cleaned[mask]
-    print(X_cleaned.shape, y_cleaned.shape)
+    X_train_backup = X_train.copy()
+    X_train = sklearn.preprocessing.StandardScaler().fit(X_train_backup).transform(X_train)
+
+    X_test = pd.read_csv(os.path.join("data", "X_test.csv"),
+                         delimiter=',',
+                         index_col='id')
+    X_test_backup = X_test
+    X_test = X_test.drop(idx, axis=1)
+    X_test = imputer.fit(X_cleaned).transform(X_test)
+    X_test = selector.transform(X_test)
+    X_test_stdised = sklearn.preprocessing.StandardScaler().fit(X_train_backup).transform(X_test)
+    # X_test_stdised = np.delete(X_test_stdised, cols, axis=1)
+
+    """
+    n_train = int(len(X_train) * .8)
+    ftrain_svm = open(os.path.join('data', 'train.svm.txt'), 'wb')
+    dump_svmlight_file(X_train.to_numpy()[:n_train], y_train.to_numpy()[:n_train], ftrain_svm)
+    ftrain_svm.close()
+
+    fval_svm = open(os.path.join('data', 'val.svm.txt'), 'wb')
+    dump_svmlight_file(X_train.to_numpy()[n_train:], y_train.to_numpy()[n_train:], fval_svm)
+    fval_svm.close()
+
+    ftest_svm = open(os.path.join('data', 'test.svm.txt'), 'wb')
+    dump_svmlight_file(X_test.to_numpy(), [0] * len(X_test), ftest_svm)
+    ftest_svm.close()
+    """
+    print(X_train.shape, y_train.shape)
+
+    # RFR_model = RandomForestRegressor(max_depth=10, n_estimators=50)
+    model = GradientBoostingRegressor(n_estimators=300, learning_rate=0.1,
+                                       max_depth=5, max_features=0.3,
+                                       min_samples_leaf=15, min_samples_split=10)
+    model.fit(X_train, y_train.ravel())
+    y_pred_RFR = model.predict(X_test_stdised)
+    submission = pd.DataFrame(y_pred_RFR, index=X_test_backup.index, columns=["y"])
+    submission.to_csv('out.csv', index=True)
+
+    """
+    xgb_model = XGBRegressor()
+    xgb_model.fit(X_train, y_train)
+
+    ans = xgb_model.predict(X_test_stdised)
+    ftest_csv = open("out.csv", "r")
+    fpred_out = open("xgb.csv", "w")
+
+    for idx, line in enumerate(ftest_csv.readlines(), -1):
+        if idx == -1:
+            fpred_out.write("%s" % line)
+        else:
+            fpred_out.write('%s,%f\n' % (line.split(",")[0], ans[idx]))
+    ftest_csv.close()
+    fpred_out.close()
+    """
 
     """
     X_cleaned_std = sklearn.preprocessing.StandardScaler().fit(X_cleaned).transform(X_cleaned)
@@ -62,67 +136,4 @@ if __name__ == '__main__':
     df_new = df.drop(columns=columnnames)
     X_train = df_new.drop(columns='y')
     y_train = df_new['y']
-    """
-
-    X_train = X_cleaned
-    y_train = y_cleaned
-
-    # lsvr = LinearSVR(C=0.01).fit(X_train, y_train)
-    model = ExtraTreesRegressor(n_estimators=50).fit(X_train, y_train.ravel())
-    selector = SelectFromModel(model, prefit=True)
-    selector.fit(X_train, y_train.ravel())
-    # selector = SelectKBest(score_func=mutual_info_regression, k=200)
-
-    X_train = sklearn.preprocessing.StandardScaler().fit(X_cleaned).transform(X_train)
-    X_train = selector.transform(X_train)
-
-    X_test = pd.read_csv(os.path.join("data", "X_test.csv"),
-                         delimiter=',',
-                         index_col='id')
-
-    X_test_KNN = imputer.fit(X_cleaned).transform(X_test)
-    X_test = pd.DataFrame(X_test_KNN, columns=X_test.columns, index=X_test.index)
-
-    X_test_stdised = sklearn.preprocessing.StandardScaler().fit(X_cleaned).transform(X_test)
-    # X_test_stdised = np.delete(X_test_stdised, cols, axis=1)
-    X_test_stdised = selector.transform(X_test_stdised)
-
-    """
-    n_train = int(len(X_train) * .8)
-    ftrain_svm = open(os.path.join('data', 'train.svm.txt'), 'wb')
-    dump_svmlight_file(X_train.to_numpy()[:n_train], y_train.to_numpy()[:n_train], ftrain_svm)
-    ftrain_svm.close()
-
-    fval_svm = open(os.path.join('data', 'val.svm.txt'), 'wb')
-    dump_svmlight_file(X_train.to_numpy()[n_train:], y_train.to_numpy()[n_train:], fval_svm)
-    fval_svm.close()
-
-    ftest_svm = open(os.path.join('data', 'test.svm.txt'), 'wb')
-    dump_svmlight_file(X_test.to_numpy(), [0] * len(X_test), ftest_svm)
-    ftest_svm.close()
-    """
-
-    RFR_model = RandomForestRegressor(max_depth=10, n_estimators=50)
-    RFR_model.fit(X_train, y_train.ravel())
-
-    y_pred_RFR = RFR_model.predict(X_test_stdised)
-    submission = pd.DataFrame(y_pred_RFR, index=X_test.index, columns=['y'])
-
-    submission.to_csv('out.csv', index=True)
-
-    """
-    xgb_model = XGBRegressor()
-    xgb_model.fit(X_train, y_train)
-
-    ans = xgb_model.predict(X_test_stdised)
-    ftest_csv = open("out.csv", "r")
-    fpred_out = open("xgb.csv", "w")
-
-    for idx, line in enumerate(ftest_csv.readlines(), -1):
-        if idx == -1:
-            fpred_out.write("%s" % line)
-        else:
-            fpred_out.write('%s,%f\n' % (line.split(",")[0], ans[idx]))
-    ftest_csv.close()
-    fpred_out.close()
     """
